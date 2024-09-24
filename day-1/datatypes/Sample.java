6942cd3e98094a7ebce81d78c53e9d38fca879e0
Naveenalla3000 <naveenalla300@gmail.com>
2024-09-25 03:07:43 +0530

    day 1

---
+++ day-1/datatypes/Sample.java
@@ -0,0 +1,29 @@
+/**
+ * Sample
+ * Author: Naveen
+ */
+import java.lang.*;
+import java.util.*;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        int  a= 32;
+        float b = 32.5f;
+        double c = 35.5;
+        long d = 12345l;
+
+        System.out.println("Number: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Long: " + d);
+
+
+        char ch = 'n';
+        System.out.println("Char: " + ch);
+
+
+        String name = "Naveen";
+        System.out.println("Name: " + name);
+    }    
+}
